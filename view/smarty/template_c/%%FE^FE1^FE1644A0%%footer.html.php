3258a657c60380a50ba342b85620fe7a54e3c0e8
xu xiaolong <bupt.xxl.qinfen@gmail.com>
2014-08-15 23:54:56 +0800

    remove view files

--- view/smarty/template_c/%%FE^FE1^FE1644A0%%footer.html.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2014-08-14 07:51:54
-         compiled from common/footer.html */ ?>
-<div class="ui-layout-south">
-    <div id="copyright">Copyright&copy;东信北邮信息技术有限公司</div>
-</div>
-</body>
-</html>
