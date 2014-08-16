701449d3d7d2401c8a213cbe6a1863691378697a
xu xiaolong <bupt.xxl.qinfen@gmail.com>
2014-08-16 10:45:54 +0800

    drop db if exist

--- deploy/db_setup/create_db.sql
+++ deploy/db_setup/create_db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF exists `asmac`;
 CREATE DATABASE `asmac` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 grant all on asmac.* to 'root' identified by '123456';
 flush privileges;
