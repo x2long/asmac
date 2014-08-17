392d345061167d4339261bd22d0a354c55826cbe
xu xiaolong <bupt.xxl.qinfen@gmail.com>
2014-08-17 23:58:11 +0800

    create Ar for province match area_code table

---
+++ protected/models/CfgProvinceAreaHelper.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ramder
+ * Date: 14-7-5
+ * Time: 上午10:17
+ * To change this template use File | Settings | File Templates.
+ */
+class CfgProvinceAreaHelper extends CfgProvinceAreaAr{
+    public $model;
+
+    public function CfgProvinceAreaHelper(){
+        $this->model = CfgProvinceAreaAr::model();
+    }
+
+    public static function getProvinceNameArray(){
+        try{
+            $db = yii::app()->db;
+            $mysql="SELECT DISTINCT province_name from cfg_province_area";
+            $command = $db->createCommand($mysql);
+            $ret = $command->queryAll();
+            return $ret;
+        }catch(Exception $e) {
+            return "未知";
+        }
+    }
+
+    public static function get_province_name_by_area_code($area_code){
+        $query_record = parent::model()->find('area_code = "'.$area_code.'"');
+        if( empty( $query_record) )
+            return "未知";
+        return $query_record->province_name;
+    }
+
+    public static function get_area_code_array_by_province_name($province_name){
+        try{
+            $db = yii::app()->db;
+            $mysql="SELECT DISTINCT area_code from cfg_province_area where province_name = '$province_name'";
+            $command = $db->createCommand($mysql);
+            $ret = $command->queryAll();
+            return $ret;
+        }catch(Exception $e) {
+            return array();
+        }
+    }
+}
